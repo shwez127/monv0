e3101460480cde522506f666c42afe5f1ba60213
Nawaz Sharieff <nawaz.sharieff@valtech.com>
2023-03-28 14:39:10 +0530

    Login Registration added

---
+++ Deskify_3.1/DeskBusiness/Services/QRScannerService.cs
@@ -0,0 +1,57 @@
+﻿using DeskData.Repository;
+using DeskEntity.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeskBusiness.Services
+{
+    public class QRScannerService
+    {
+        IQRScannerRepository _qScannerRepository;
+        public QRScannerService(IQRScannerRepository qScannerRepository)
+        {
+            _qScannerRepository = qScannerRepository;
+        }
+
+        #region Serice for AddQScanner
+
+        public void AddQScanner(QRScanner qScanner)
+        {
+            _qScannerRepository.AddQScanner(qScanner);
+        }
+        #endregion Serice for AddQScanner
+
+        #region Serice for UpdateQScanner
+
+        public void UpdateQScanner(QRScanner qScanner)
+        {
+            _qScannerRepository.UpdateQScanner(qScanner);
+        }
+        #endregion Serice for UpdateQScanner
+
+        #region Serice for DeleteQScanner
+
+        public void DeleteQScanner(int qScannerId)
+        {
+            _qScannerRepository.DeleteQScanner(qScannerId);
+        }
+        #endregion Serice for AddQScanner
+
+        #region Serice for GetQScannerById
+
+        public QRScanner GetQScannerById(int qScannerId)
+        {
+            return _qScannerRepository.GetQScannerById(qScannerId);
+        }
+        #endregion Serice for GetQScannerById
+
+        #region Serice for GetQScanner
+
+        public IEnumerable<QRScanner> GetQScanner()
+        {
+            return _qScannerRepository.GetQScanner();
+        }
+        #endregion Serice for GetQScanner
+    }
+}
---
+++ Deskify_3.1/DeskUI/Controllers/LoginTableController.cs
@@ -0,0 +1,78 @@
+﻿using DeskEntity.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeskUI.Controllers
+{
+    public class LoginTableController : Controller
+    {
+        private IConfiguration _configuration;
+        public LoginTableController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginTable loginTable)
+        {
+            #region Admin and employee can login
+            ViewBag.status = "";
+            if (loginTable.Email == null && loginTable.Password == null)
+            {
+                ViewBag.status = "Error";
+                ViewBag.message = "Email and Password is required";
+
+            }
+            else
+            {
+                int[] arr = new int[2];
+                using (HttpClient client = new HttpClient())
+                {
+                    StringContent content = new StringContent(JsonConvert.SerializeObject(loginTable), Encoding.UTF8, "application/json");
+                    string endpoint = _configuration["WebApiBaseUrl"] + "LoginTable/Login";
+                    using (var response = await client.PostAsync(endpoint, content))
+                    {
+                        var result = await response.Content.ReadAsStringAsync();
+                        arr = JsonConvert.DeserializeObject<int[]>(result);
+
+                        TempData.Keep();
+                        if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                        {
+                            TempData["ProfileId"] = arr[0].ToString();
+                            TempData.Keep();
+                            ViewBag.status = "Ok";
+                            ViewBag.message = "Login Succesfully";
+                            if (arr[1] == 0)
+                            {
+                                TempData["EmployeeID"] = arr[0].ToString();
+                                TempData.Keep();
+
+                                return RedirectToAction("Index", "Employee");
+                            }
+                            else if (arr[1] == 3)
+                            {
+                                return RedirectToAction("Index", "Admin");
+                            }
+
+                        }
+                        else
+                        {
+                            ViewBag.status = "Error";
+                            ViewBag.message = "Invalid Email Address/Password";
+                        }
+                    }
+                }
+            }
+            return View();
+            #endregion
+        }
+    }
+}
