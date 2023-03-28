7c7a51affff8fc3730b6705d2002dfc266cc1b6a
Ankesh Raj <ankesh.raj@valtech.com>
2023-03-28 14:46:19 +0530

    Employee Added

--- Deskify_3.1/DeskData/Data/DeskDbContext.cs
+++ Deskify_3.1/DeskData/Data/DeskDbContext.cs
@@ -33,7 +33,7 @@ namespace DeskData.Data
         protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
         {
 
-           dbContextOptionsBuilder.UseSqlServer("Data Source=DESKTOP-SCHFIU6\\SQLEXPRESS;Initial Catalog=deskdb; Integrated Security=true; ");
+           dbContextOptionsBuilder.UseSqlServer("Data Source=DESKTOP-8E9A3F4\\SQLEXPRESS;Initial Catalog=deskdb; Integrated Security=true; ");
 
         }
     }
---
+++ Deskify_3.1/DeskData/Repository/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using DeskEntity.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeskData.Repository
+{
+    public interface IEmployeeRepository
+    {
+        void AddEmployee(Employee employee);
+        void DeleteEmployee(int employeeId);
+        void UpdateEmployee(Employee employee);
+        Employee GetEmployeeById(int employeeId);
+        IEnumerable<Employee> GetAllEmployees();
+    }
+}
