90287ae804040d73da624037d238402bd8d9a7e4
Ritik Sharan <ritik.sharan@valtech.com>
2023-03-30 10:36:21 +0530

    added database schema and updated admin mvc

--- Deskify_3.1/DeskUI/Controllers/AdminController.cs
+++ Deskify_3.1/DeskUI/Controllers/AdminController.cs
@@ -1,12 +1,165 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DeskEntity.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using Microsoft.Extensions.Configuration;
 
 namespace DeskUI.Controllers
 {
     public class AdminController : Controller
     {
+        private IConfiguration _configuration;
+
+        public AdminController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        #region Room Add-View-Edit-Delete Actions
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRooms()
+        {
+            IEnumerable<Room> roomresult = null;
+            using (HttpClient client = new HttpClient())
+            {
+                // LocalHost Adress in endpoint
+                string endPoint = _configuration["WebApiBaseUrl"] + "Room/GetAllRooms";
+                using (var response = await client.GetAsync(endPoint))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        var result = await response.Content.ReadAsStringAsync();
+                        //It will deserilize the object in the form of JSON
+                        roomresult = JsonConvert.DeserializeObject<IEnumerable<Room>>(result);
+                    }
+                }
+            }
+            return View(roomresult);
+        }
+
+        public IActionResult AddRoom()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRoom(Room room)
+        {
+            ViewBag.status = "";
+            //it will update the room details after Admin Changes
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(room), Encoding.UTF8, "application/json");
+                string endPoint = _configuration["WebApiBaseUrl"] + "Room/AddRoom";
+                using (var response = await client.PostAsync(endPoint, content))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        ViewBag.status = "Ok";
+                        ViewBag.message = "Room Details Added Successfully!";
+                        //return RedirectToAction("Index", "Room");
+                    }
+                    else
+                    {
+                        ViewBag.status = "Error";
+                        ViewBag.message = "Wrong Entries!";
+                    }
+                }
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> UpdateRoom(int roomId)
+        {
+            if (roomId != 0)
+            {
+                //We are Storing Doctor Id  temporary to avoid the error. Now it will show the room details after the update also
+                TempData["UpdateroomId"] = roomId;
+                TempData.Keep();
+            }
+            Room room = null;
+            //it will fetch the room Details by using DoctorID
+            using (HttpClient client = new HttpClient())
+            {
+                string endPoint = _configuration["WebApiBaseUrl"] + "Room/GetRoomsById?roomId=" + Convert.ToInt32(TempData["EditroomId"]);
+                TempData.Keep();
+                using (var response = await client.GetAsync(endPoint))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        var result = await response.Content.ReadAsStringAsync();
+                        //It will deserilize the object in the form of JSON
+                        room = JsonConvert.DeserializeObject<Room>(result);
+                    }
+                }
+            }
+            return View(room);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateRoom(Room room)
+        {
+            ViewBag.status = "";
+            //it will update the room details after Admin Changes
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(room), Encoding.UTF8, "application/json");
+                string endPoint = _configuration["WebApiBaseUrl"] + "Room/UpdateRoom";
+                using (var response = await client.PutAsync(endPoint, content))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        ViewBag.status = "Ok";
+                        ViewBag.message = "Room Details Updated Successfully!";
+                        //return RedirectToAction("GetAllRooms", "Room");
+                    }
+                    else
+                    {
+                        ViewBag.status = "Error";
+                        ViewBag.message = "Wrong Entries!";
+                    }
+                }
+            }
+            return View();
+
+        }
+
+        public async Task<IActionResult> DeleteRoom(int roomId)
+        {
+            ViewBag.status = "";
+            //it will Delete the doctor Details by using doctor Id
+            using (HttpClient client = new HttpClient())
+            {
+                string endPoint = _configuration["WebApiBaseUrl"] + "Room/DeleteRoom?roomId=" + roomId;
+                using (var response = await client.DeleteAsync(endPoint))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        ViewBag.status = "Ok";
+                        ViewBag.message = "Room Details Deleted Successfully!";
+                        return RedirectToAction("Index", "Room");
+                    }
+                    else
+                    {
+                        ViewBag.status = "Error";
+                        ViewBag.message = "Wrong Entries!";
+                    }
+                }
+            }
+            return View();
+
+        }
+
+        #endregion
+
     }
 }
